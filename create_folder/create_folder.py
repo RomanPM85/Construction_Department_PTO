617a61e3c35c8897df668d1fcbc82a1e6024c976
RomanPM85 <mamchiy@yandex.ru>
2023-05-26 02:12:24 +0300

    added a function to create registries in each directory.

--- create_folder/create_folder.py
+++ create_folder/create_folder.py
@@ -25,37 +25,58 @@ def folders_tree():
             print("Файл:", os.path.join(dirPath, fileName))
 
 
+def create_files(filenames, text_body):
+    with open(filenames, 'a', encoding='utf-8') as f:
+        f.write(text_body)
+
+
 if __name__ == "__main__":
     """ 
-    Тестовые переменные
+    Запуск команд для создание начальной структуры папок и файлов для реестра.
     """
+    mainFolder = os.getcwd()
     dict_dirs = {}
     foldersStructure = [
-        ('01_Договор', '02_ДопСоглашение', '03_НаСогласовании', '04_Исходники'),
-        ('02_Приказы', 'level_1', 'level_2', 'level_3'),
-        ('03_Письма', 'level_1', 'level_2', 'level_3'),
-        ('04_Рабочая_документация', 'level_1', 'level_2', 'level_3'),
-        ('05_Исполнительная_документация', 'level_1', 'level_2', 'level_3'),
-        ('06_КС6_Накопительная_ведомость', 'level_1', 'level_2', 'level_3'),
-        ('07_КС2_КС3', 'level_1', 'level_2', 'level_3'),
-        ('08_Журналы', 'level_1', 'level_2', 'level_3'),
-        ('09_Техника_Безопасности', 'level_1', 'level_2', 'level_3'),
-        ('10_Чек_Листы', 'level_1', 'level_2', 'level_3'),
-        ('11_Накладные', 'level_1', 'level_2', 'level_3'),
-        ('12_Акты', 'level_1', 'level_2', 'level_3'),
+        ("01_Договор", "ДопСоглашение"),
+        ("02_Приказы", "Заказчик"),
+        ("03_ТехникаБезопасности",),
+        ("04_Журналы",),
+        ("05_Сотрудники",),
+        ("06_Письма",),
+        ("07_Накладные",),
+        ("08_РабочаяДокументация",),
+        ("09_ИсполнительнаяДокументация",),
+        ("10_КС6_НакопительнаяВедомость",),
+        ("11_КС2_КС3",),
+        ("12_ЧекЛисты",),
+        ("13_Акты",),
     ]
+    body_text = "Hello world!"
     start = int(input(f"Введите команду:\n "
-                      f"создать=1\n "
-                      f"удалить=2\n "
-                      f"список папок и файлов=3\n"
-                      f" >>"))
+                      f"Создать структуру папок=1\n "
+                      f"Удалить все папки=2\n "
+                      f"Распечатать список папок и файлов=3\n"
+                      f"Создать реестры=4\n"
+                      f">>"))
     if start == 1:
         for i in foldersStructure:
             create_folder(i)
+
     elif start == 2:
         delete_folder()
+
     elif start == 3:
         folders_tree()
+
+    elif start == 4:
+        list_folders = os.listdir(path='.')
+        for folder in list_folders:
+            if os.path.isdir(folder):
+                os.chdir(folder)
+                create_files('!Reestr'+folder[2:]+'.xlsx', body_text)
+                os.chdir(mainFolder)
+            else:
+                pass
     else:
         print(f"Завершение программы")
 
