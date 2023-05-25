41c9dd45123a33ee12642a734d804d9dcd645622
RomanPM85 <mamchiy@yandex.ru>
2023-05-25 21:33:22 +0300

    update fun create_folder(), create fun folders_tree()

--- create_folder/create_folder.py
+++ create_folder/create_folder.py
@@ -2,42 +2,61 @@ import os
 import shutil
 
 
-def create_folder():
-    os.makedirs(os.path.join('01_Договор', 'level_1', 'level_2', 'level_3'))
-    os.makedirs(os.path.join('02_Приказы', 'level_1', 'level_2', 'level_3'))
-    os.makedirs(os.path.join('03_Письма', 'level_1', 'level_2', 'level_3'))
-    os.makedirs(os.path.join('04_Рабочая_документация', 'level_1', 'level_2', 'level_3'))
-    os.makedirs(os.path.join('05_Исполнительная_документация', 'level_1', 'level_2', 'level_3'))
-    os.makedirs(os.path.join('06_КС6_Накопительная_ведомость', 'level_1', 'level_2', 'level_3'))
-    os.makedirs(os.path.join('07_КС2_КС3', 'level_1', 'level_2', 'level_3'))
-    os.makedirs(os.path.join('08_Журналы', 'level_1', 'level_2', 'level_3'))
-    os.makedirs(os.path.join('09_Техника_Безопасности', 'level_1', 'level_2', 'level_3'))
-    os.makedirs(os.path.join('10_Чек_Листы', 'level_1', 'level_2', 'level_3'))
-    os.makedirs(os.path.join('11_Накладные', 'level_1', 'level_2', 'level_3'))
-    os.makedirs(os.path.join('12_Акты', 'level_1', 'level_2', 'level_3'))
-    os.makedirs(os.path.join('13_Замечания', 'level_1', 'level_2', 'level_3'))
-
-
-def new_create_folder(*args):
-    for i in args:
-        os.makedirs(os.path.join(i))
+def create_folder(*args):
+    for item in args:
+        os.makedirs(os.path.join(*item))
 
 
 def delete_folder():
-    try:
-        shutil.rmtree(os.path.join('01_folder'))
-        shutil.rmtree(os.path.join('02_folder'))
-    except FileNotFoundError:
-        pass
+    path = '.'
+    dirs = os.listdir(path)
+    for item in dirs:
+        if os.path.isdir(str(item)):
+            shutil.rmtree(os.path.join(item))
+
+
+def folders_tree():
+    for dirPath, dirNames, filenames in os.walk("."):
+        # перебрать каталоги
+        for dirName in dirNames:
+            print("Каталог:", os.path.join(dirPath, dirName))
+        # перебрать файлы
+        for fileName in filenames:
+            print("Файл:", os.path.join(dirPath, fileName))
 
 
 if __name__ == "__main__":
+    """ 
+    Тестовые переменные
+    """
     dict_dirs = {}
-    list_dirs = ['01_Договор', 'level_1', 'level_2', 'level_3']
-    start = int(input(f"Введите команду:\n создать=1\n удалить=2\n >>"))
+    foldersStructure = [
+        ('01_Договор', '02_ДопСоглашение', '03_НаСогласовании', '04_Исходники'),
+        ('02_Приказы', 'level_1', 'level_2', 'level_3'),
+        ('03_Письма', 'level_1', 'level_2', 'level_3'),
+        ('04_Рабочая_документация', 'level_1', 'level_2', 'level_3'),
+        ('05_Исполнительная_документация', 'level_1', 'level_2', 'level_3'),
+        ('06_КС6_Накопительная_ведомость', 'level_1', 'level_2', 'level_3'),
+        ('07_КС2_КС3', 'level_1', 'level_2', 'level_3'),
+        ('08_Журналы', 'level_1', 'level_2', 'level_3'),
+        ('09_Техника_Безопасности', 'level_1', 'level_2', 'level_3'),
+        ('10_Чек_Листы', 'level_1', 'level_2', 'level_3'),
+        ('11_Накладные', 'level_1', 'level_2', 'level_3'),
+        ('12_Акты', 'level_1', 'level_2', 'level_3'),
+    ]
+    start = int(input(f"Введите команду:\n "
+                      f"создать=1\n "
+                      f"удалить=2\n "
+                      f"список папок и файлов=3\n"
+                      f" >>"))
     if start == 1:
-        create_folder()
+        for i in foldersStructure:
+            create_folder(i)
     elif start == 2:
         delete_folder()
+    elif start == 3:
+        folders_tree()
     else:
         print(f"Завершение программы")
+
+
