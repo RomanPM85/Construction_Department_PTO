055280ce00132459c80435ce4828edf77845b0f2
Roman <mamchiy@yandex.ru>
2024-03-28 11:04:48 +0300

    added the unzip zip file method

--- archives/archives.py
+++ archives/archives.py
@@ -1,14 +1,24 @@
+import zipfile
+
 import patoolib
 import time
 from pathlib import Path
 
 
-def extract_archive():
+def extract_archive_rar():
     """ Function for extracting files from rar archives in the current folder """
     path_archives = Path.cwd().rglob('*.rar')
     path_extract_archive = Path.cwd()
     for archive in path_archives:
         patoolib.extract_archive(str(archive), outdir=str(path_extract_archive))
+
+
+def extract_archive_zip():
+    path_archives = Path.cwd().rglob('*.zip')
+    path_extract_archive = Path.cwd()
+    for archive in path_archives:
+        with zipfile.ZipFile(archive, 'r') as zip_file:
+            zip_file.extractall(path_extract_archive)
 
 
 if __name__ == "__main__":
@@ -18,12 +28,15 @@ if __name__ == "__main__":
 
     print(welcome)
     start_script = input(f"Enter the program command number=>.\n"
-                         f"If 1, all archives in the current folder will be unpacked. \n"
+                         f"If 1, all archives rar in the current folder will be unpacked. \n"
+                         f"If 2, all archives zip in the current folder will be unpacked. \n"
                          f"==>"
                          )
 
     if start_script == '1':
-        extract_archive()
+        extract_archive_rar()
+    elif start_script == '2':
+        extract_archive_zip()
     else:
-        print(f"код введен неверно")
+        print(f"the code is not entered")
 print("--- %s seconds ---" % (time.time() - program_start_time))
