f45fbc274d033a465be2b10d3e8956c1af56df79
Roman <mamchiy@yandex.ru>
2024-09-12 00:26:24 +0300

    chapter-1

--- BeatifulSoup/BeatifulSoup-test.py
+++ BeatifulSoup/BeatifulSoup-test.py
@@ -1,10 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Современный скрапинг веб-сайтов с помощью Python "Райан Митчелл" 2021
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from urllib.error import HTTPError
 
-try:
-    html = urlopen('https://technical.city/ru/cpu/intel-rating')
-    bs = BeautifulSoup(html.read(), 'html.parser')
-    print(bs.h1)
-except HTTPError as error:
-    print(error)
+
+def get_title(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        title = bs.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+
+if __name__ == "__main__":
+    title = get_title('https://technical.city/ru/cpu/intel-rating')
+
+    if title is None:
+        print('title cold no be found')
+    else:
+        print(title)
