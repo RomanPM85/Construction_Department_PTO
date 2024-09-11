c548d63d4f496e2bbf5ac04613fd376648d1f91c
Roman <mamchiy@yandex.ru>
2024-09-12 00:59:55 +0300

    chapter-2

--- BeatifulSoup/BeatifulSoup-test.py
+++ BeatifulSoup/BeatifulSoup-test.py
@@ -6,23 +6,11 @@ from bs4 import BeautifulSoup
 from urllib.error import HTTPError
 
 
-def get_title(url):
-    try:
-        html = urlopen(url)
-    except HTTPError as e:
-        return None
-    try:
-        bs = BeautifulSoup(html.read(), 'html.parser')
-        title = bs.body.h1
-    except AttributeError as e:
-        return None
-    return title
-
-
 if __name__ == "__main__":
-    title = get_title('https://technical.city/ru/cpu/intel-rating')
+    # title = get_title('https://technical.city/ru/cpu/intel-rating')
+    html = urlopen('http://www.pythonscraping.com/pages/warandpeace.html')
+    bs = BeautifulSoup(html.read(), 'html.parser')
+    name_list = bs.find_all('span', {'class': 'green'})
+    for name in name_list:
+        print(name.get_text())
 
-    if title is None:
-        print('title cold no be found')
-    else:
-        print(title)
