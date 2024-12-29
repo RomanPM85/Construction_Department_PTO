0db46b559020f20b65a7610e1720c8f7f628f738
Roman <mamchiy@yandex.ru>
2024-12-30 02:46:34 +0300

    python_script

--- python_script/python_script.py
+++ python_script/python_script.py
@@ -1,7 +1,19 @@
 import subprocess
+import os
 from pathlib import Path
 
-path_file = Path.cwd() / "dir" / "file.py"
+path_dir_abs = Path.cwd()
 
-result = subprocess.run(['python', path_file])
+path_dirs_local = [
+    "folder/folder/folder/folder/",
+    "folder/folder/folder/",
+    "folder//folder/",
+    "folder/"
+]
 
+
+for dir_local in path_dirs_local:
+    file = "test.py"
+    local_item_dir = path_dir_abs / dir_local
+    os.chdir(local_item_dir)
+    subprocess.run(['python', file])
