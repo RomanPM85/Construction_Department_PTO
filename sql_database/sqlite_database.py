ffe1e573f6b2630203c774829d597668a7a2845b
Roman <mamchiy@yandex.ru>
2025-01-11 23:54:48 +0300

    sqlite_database

--- sql_database/sqlite_database.py
+++ sql_database/sqlite_database.py
@@ -1,57 +1,55 @@
 # -*- encoding: utf-8 -*-
+
 import pandas as pd
 import sqlite3
 from pathlib import Path
-
+from datetime import date
 
 path_dir_abs = Path.cwd()
-file_path = path_dir_abs /'xlsx_database.xlsx'
-sheet_name = 'date'
-
+file_path = path_dir_abs / 'xlsx_database.xlsx'
+sheet_name = 'message_database'
 data = pd.read_excel(file_path, sheet_name=sheet_name)
+
+
 connection = sqlite3.connect('xlsx_database.sqlite')
 cursor = connection.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS medical_center (
-id INTEGER PRIMARY KEY, 
-task TEXT NOT NULL, 
-status INTEGER, 
+cursor.execute('''CREATE TABLE IF NOT EXISTS message_database (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+task_name TEXT NOT NULL,
 stage TEXT NOT NULL,
-message_date DATE NOT NULL, 
-final_date DATE NOT NULL
+status INTEGER,
+date_message DATE NOT NULL,
+recipients TEXT NOT NULL,
+copy_recipients TEXT NOT NULL,
+result_date DATE NOT NULL
 )''')
 
-data.to_sql('medical_center', connection, if_exists='replace', index=False)
-
-
-def filter_date() -> list:
-    cursor.execute('SELECT task, status, final_date FROM medical_center '
-                   'WHERE final_date < ?', ('2024-09-01',))
-    final_date = cursor.fetchall()
-    return final_date
-
-
-def get_select() -> list:
-    # cursor.execute("SELECT status, final_date FROM medical_center WHERE status=1")
-    cursor.execute("SELECT task, final_date FROM medical_center "
-                   "WHERE final_date<'2024-09-01' "
-                   "AND status=1 "
-                   "AND stage='ИРД' "
-                   "AND final_date>'2024-07-09'")
-    select_all = cursor.fetchall()
-    return select_all
-
-
-get_list_1 = filter_date()
-get_list_2 = get_select()
+data.to_sql('message_database', connection, if_exists='replace', index=False)
 connection.close()
 
 
-for i in get_list_2:
-    print(i)
+def filter_selected_query(query, *args) -> list:
+    conn = sqlite3.connect('xlsx_database.sqlite')
+    cur = conn.cursor()
+    cur.execute(query, args)
+    select_all = cur.fetchall()
+    conn.close()
+    return select_all
 
-print(type(get_list_2))
-for i in get_list_1:
-    print(i)
-print(type(get_list_1))
 
+if __name__ == "__main__":
+    status_2 = 0
+    id_2 = 42
+    recipients_2 = 'recipients_2@company.ru'
+    result_date_2 = date.today().strftime("%Y-%m-%d")
+    query_2 = ("SELECT id, recipients, task_name, date_message, result_date FROM message_database "
+             "WHERE status = ? "
+             "AND recipients = ? "
+             "AND id = ? "
+             "AND result_date > ?")
+
+    get_list_2 = filter_selected_query(query_2, status_2, recipients_2, id_2, result_date_2)
+
+    for i in get_list_2:
+        print(i)
